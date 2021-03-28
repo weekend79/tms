c9b6cb692354afaa422fcd7b88add33593f6bd05
weekend79 <mortenviken@yahoo.com>
2021-03-28 11:54:51 +0000

    Updated settings and url files

--- tms/settings.py
+++ tms/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
+    'home',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +76,17 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+LOGIN_REDIRECT_URL = 'home'
 
 WSGI_APPLICATION = 'tms.wsgi.application'
 
